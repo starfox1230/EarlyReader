8c9f0169ca860e62906213066131567291d21b84
Sterling <sterling32157@yahoo.com>
2023-05-29 15:18:04 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById('pasteStoryButton').addEventListener('click', function() {
+    var text = document.getElementById('storyArea').value;
+    var sentences = text.split(/(?<=[.!?])\s+(?=[a-z"])/i);
+    sentences = sentences.map(function(sentence) {
+        return sentence.trim().split(" ");
+    });
+    var currentSentenceIndex = 0;
+    var currentWordIndex = 0;
+
+    function updateSentence() {
+        var sentenceDisplay = document.getElementById('sentenceDisplay');
+        sentenceDisplay.innerHTML = '';
+        sentences[currentSentenceIndex].forEach(function(word, index) {
+            if (index === currentWordIndex) {
+                sentenceDisplay.innerHTML += '<span class="highlight">' + word + '</span> ';
+            } else {
+                sentenceDisplay.innerHTML += word + ' ';
+            }
+        });
+    }
+
+    function prevWord() {
+        if (currentWordIndex > 0) {
+            currentWordIndex -= 1;
+        } else if (currentSentenceIndex > 0) {
+            currentSentenceIndex -= 1;
+            currentWordIndex = sentences[currentSentenceIndex].length - 1;
+        }
+        updateSentence();
+    }
+
+    function nextWord() {
+        if (currentWordIndex < sentences[currentSentenceIndex].length - 1) {
+            currentWordIndex += 1;
+        } else if (currentSentenceIndex < sentences.length - 1) {
+            currentSentenceIndex += 1;
+            currentWordIndex = 0;
+        }
+        updateSentence();
+    }
+
+    document.getElementById('prevWordButton').addEventListener('click', prevWord);
+    document.getElementById('nextWordButton').addEventListener('click', nextWord);
+
+    updateSentence();
+});
