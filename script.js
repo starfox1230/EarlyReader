432bfefb8fe3b7367e6e5df5f7733c1f195b6c56
Sterling <sterling32157@yahoo.com>
2023-06-10 01:59:21 -0400

    Now the previous and next buttons, as well as the output text box, are not visible until after you insert the story.

--- script.js
+++ script.js
@@ -1,74 +1,84 @@
-var text, sentences, currentSentenceIndex, currentWordIndex;
-var isStoryInserted = false;
-
-function updateSentence() {
-    var sentenceDisplay = document.getElementById('sentenceDisplay');
-    sentenceDisplay.innerHTML = '';
-    sentences[currentSentenceIndex].forEach(function(word, index) {
-        if (index === currentWordIndex) {
-            sentenceDisplay.innerHTML += '<span class="highlight">' + word + '</span> ';
-        } else {
-            sentenceDisplay.innerHTML += word + ' ';
-        }
-    });
-}
-
-function prevWord() {
-    if (!isStoryInserted) return;
-    if (currentWordIndex > 0) {
-        currentWordIndex -= 1;
-    } else if (currentSentenceIndex > 0) {
-        currentSentenceIndex -= 1;
-        currentWordIndex = sentences[currentSentenceIndex].length - 1;
-    }
-    updateSentence();
-}
-
-function nextWord() {
-    if (!isStoryInserted) return;
-    if (currentWordIndex < sentences[currentSentenceIndex].length - 1) {
-        currentWordIndex += 1;
-    } else if (currentSentenceIndex < sentences.length - 1) {
-        currentSentenceIndex += 1;
-        currentWordIndex = 0;
-    }
-    updateSentence();
-}
-
-document.getElementById('pasteStoryButton').addEventListener('click', function() {
-    text = document.getElementById('storyArea').value;
-    sentences = text.split(/(?<=[.!?])\s+(?=[a-z"])/i);
-    sentences = sentences.map(function(sentence) {
-        return sentence.trim().split(" ");
-    });
-    currentSentenceIndex = 0;
-    currentWordIndex = 0;
-    isStoryInserted = true;
-
-    // Use 'touchstart' event for more responsive interaction on touch devices
-    document.getElementById('prevWordButton').addEventListener('touchstart', function(e) {
-        e.preventDefault(); // Prevents the browser's default touch behavior
-        prevWord();
-    });
-    document.getElementById('prevWordButton').addEventListener('click', prevWord);
-
-    document.getElementById('nextWordButton').addEventListener('touchstart', function(e) {
-        e.preventDefault(); // Prevents the browser's default touch behavior
-        nextWord();
-    });
-    document.getElementById('nextWordButton').addEventListener('click', nextWord);
-
-    // Hide the Insert Story button and text area after the button is clicked
-    document.getElementById('pasteStoryButton').style.display = 'none';
-    document.getElementById('storyArea').style.display = 'none';
-
-    updateSentence();
-});
-
-window.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowRight") {
-        nextWord();
-    } else if (e.key === "ArrowLeft") {
-        prevWord();
-    }
-});
+window.onload = function() {
+    document.getElementById('buttons').style.display = 'none';
+    document.getElementById('sentenceDisplay').style.display = 'none';
+  };
+  
+  var text, sentences, currentSentenceIndex, currentWordIndex;
+  var isStoryInserted = false;
+  
+  function updateSentence() {
+      var sentenceDisplay = document.getElementById('sentenceDisplay');
+      sentenceDisplay.innerHTML = '';
+      sentences[currentSentenceIndex].forEach(function(word, index) {
+          if (index === currentWordIndex) {
+              sentenceDisplay.innerHTML += '<span class="highlight">' + word + '</span> ';
+          } else {
+              sentenceDisplay.innerHTML += word + ' ';
+          }
+      });
+  }
+  
+  function prevWord() {
+      if (!isStoryInserted) return;
+      if (currentWordIndex > 0) {
+          currentWordIndex -= 1;
+      } else if (currentSentenceIndex > 0) {
+          currentSentenceIndex -= 1;
+          currentWordIndex = sentences[currentSentenceIndex].length - 1;
+      }
+      updateSentence();
+  }
+  
+  function nextWord() {
+      if (!isStoryInserted) return;
+      if (currentWordIndex < sentences[currentSentenceIndex].length - 1) {
+          currentWordIndex += 1;
+      } else if (currentSentenceIndex < sentences.length - 1) {
+          currentSentenceIndex += 1;
+          currentWordIndex = 0;
+      }
+      updateSentence();
+  }
+  
+  document.getElementById('pasteStoryButton').addEventListener('click', function() {
+      text = document.getElementById('storyArea').value;
+      sentences = text.split(/(?<=[.!?])\s+(?=[a-z"])/i);
+      sentences = sentences.map(function(sentence) {
+          return sentence.trim().split(" ");
+      });
+      currentSentenceIndex = 0;
+      currentWordIndex = 0;
+      isStoryInserted = true;
+  
+      // Use 'touchstart' event for more responsive interaction on touch devices
+      document.getElementById('prevWordButton').addEventListener('touchstart', function(e) {
+          e.preventDefault(); // Prevents the browser's default touch behavior
+          prevWord();
+      });
+      document.getElementById('prevWordButton').addEventListener('click', prevWord);
+  
+      document.getElementById('nextWordButton').addEventListener('touchstart', function(e) {
+          e.preventDefault(); // Prevents the browser's default touch behavior
+          nextWord();
+      });
+      document.getElementById('nextWordButton').addEventListener('click', nextWord);
+  
+      // Hide the Insert Story button and text area after the button is clicked
+      document.getElementById('pasteStoryButton').style.display = 'none';
+      document.getElementById('storyArea').style.display = 'none';
+  
+      // Display the buttons and sentence display when story is inserted
+      document.getElementById('buttons').style.display = 'block';
+      document.getElementById('sentenceDisplay').style.display = 'block';
+  
+      updateSentence();
+  });
+  
+  window.addEventListener('keydown', function(e) {
+      if (e.key === "ArrowRight") {
+          nextWord();
+      } else if (e.key === "ArrowLeft") {
+          prevWord();
+      }
+  });
+  
